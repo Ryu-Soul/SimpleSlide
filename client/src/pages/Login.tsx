4726d067d93744806236ccf296ba873bf678a6a4
RyuSoul <quentin.barriere@gmail.com>
2026-03-17 09:29:39 +0100

    Add backend server setup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,22 @@
-
+import { NavLink } from "react-router-dom"
+import "../styles/login.scss"
 
 function Login () {
     return (
-        <h1>Login Page</h1>
+        <main>
+            <section className="login">
+                <h1>Page de connexion</h1>
+                <p>Blablabla</p>
+                <form>
+                    <label htmlFor="email">email</label>
+                    <input id="email" type="email" required placeholder="email@exemple.com" autoComplete="email"/>
+                    <label htmlFor="password">Mot de passe</label>
+                    <input id="password" type="password" required placeholder="••••••••" autoComplete="current-password"/>
+                    <button type="submit" className="btn">Se connecter</button>
+                    <NavLink to="/">Retour à l'accueil</NavLink>
+                </form>
+            </section>
+        </main>
     )
 }
 
